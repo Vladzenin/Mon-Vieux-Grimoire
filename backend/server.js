dac7936be144764a0bcb94d047703285b25b5f12
julien ferté <julienfertenissan@gmail.com>
2024-09-16 22:00:53 +0200

    install packagejson propre

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const http = require('http');
 const app = require('./app');
 
 const normalizePort = (val) => {
-  const port = parseInt(val, 10);
+const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
+  if (Number.isNaN(port)) {
     return val;
   }
   if (port >= 0) {
