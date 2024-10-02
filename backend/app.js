c77a3a81133a1f7e690dd2c52dbcee2f5a3122e8
julien ferté <julienfertenissan@gmail.com>
2024-10-03 01:03:04 +0200

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,32 @@
+require("dotenv").config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require ('./models/Book');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
 const app = express();
+const path = require ('path');
+
+const databaseConfig = require("./configs/databaseConfig");
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
+mongoose
+    .connect(databaseConfig.dbUri)
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
-app.use((req, res, next) => {
-   res.json({ message: 'Votre requête a bien été reçue !' });
-   next(); 
-});
+app.use(express.json());
 
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 module.exports = app;
